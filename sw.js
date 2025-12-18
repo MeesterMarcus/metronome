e1013e326cf4fb91b0a6ee58df45dc605597f431
meestermarcus <lorenzanamarcus@gmail.com>
2025-12-18 17:24:54 -0600

    metronome

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "minimal-metronome-v1";
+const ASSETS = ["./", "./index.html", "./app.webmanifest", "./sw.js", "./icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+          return res;
+        })
+        .catch(() => caches.match("./"));
+    })
+  );
+});
