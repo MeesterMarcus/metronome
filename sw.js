34584118ae94ad8eb48e8b631b1946bf7c6bdded
meestermarcus <lorenzanamarcus@gmail.com>
2025-12-18 22:12:04 -0600

    asfasdfasDF

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "minimal-metronome-v7";
+const CACHE_NAME = "minimal-metronome-v8";
 const ASSETS = ["./", "./index.html", "./app.webmanifest", "./sw.js", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
